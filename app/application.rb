4c36c8d13829c5ac72acb36ae0d45bc4cfcb0100
Joe Ganobsik <jganobsik@gmail.com>
2019-01-12 20:52:56 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,8 +17,11 @@ class Application
       resp.write handle_search(search_term)
         elsif req.path.match(/cart/)
         if @@cart.empty?
+          resp.write "Your cart is empty"
+        else 
       @@cart.each do |item|
         resp.write "#{item}\n"
+      end
       end
        elsif req.path.match(/add/)
       item = req.params["i"]
